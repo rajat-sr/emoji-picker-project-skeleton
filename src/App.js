a6d3fba99ffc3533a5ca8a4775f1067d69f12a0c
Rajat Srivastava <rajat.srivastava.20893@gmail.com>
2019-09-10 19:47:32 +0530

    Perform code cleanup

--- src/App.js
+++ src/App.js
@@ -1,79 +1,38 @@
 import React, { Component } from 'react';
 import './App.css';
+import { connect } from 'react-redux';
 import EmojiDisplayer from './components/EmojiDisplayer';
 import EmojiButton from './components/EmojiButton';
-import { connect } from 'react-redux';
 import EmojiPicker from './components/EmojiPicker';
 import TetherComponent from 'react-tether';
 import { togglePicker } from './redux/actions';
 
 class App extends Component {
   tether = null;
-  container = null;
-  attachments = ['top center', 'middle left', 'bottom center', 'middle right'];
-  state = { on: true, attachment: this.attachments[0] };
-
-  componentDidMount() {
-    // Rerender with the container ref
-    this.setState({});
-  }
-
-  toggleTooltip = () => {
-    this.setState(({ on }) => {
-      return { on: !on };
-    });
-  };
-
-  cycleAttachment = () => {
-    const { attachment } = this.state;
-    let nextAttachment = this.attachments.indexOf(attachment) + 1;
-    if (nextAttachment === this.attachments.length) {
-      nextAttachment = 0;
-    }
-    this.setState(() => ({
-      attachment: this.attachments[nextAttachment],
-    }));
-  };
 
   render() {
     return (
       <div className="App">
         <h1>Emoji Picker</h1>
         <EmojiDisplayer />
-        {
-          <div
-            ref={container => {
-              this.container = container;
-            }}
-          >
-            {this.container && (
-              <TetherComponent
-                ref={tether => {
-                  this.tether = tether;
-                }}
-                attachment={this.state.attachment}
-                constraints={[
-                  {
-                    to: this.container,
-                    attachment: 'together',
-                  },
-                ]}
-                renderTarget={ref => (
-                  <div ref={ref}>
-                    <EmojiButton />
-                  </div>
-                )}
-                renderElement={ref =>
-                  this.props.displayPicker && (
-                    <div ref={ref}>
-                      <EmojiPicker />
-                    </div>
-                  )
-                }
-              ></TetherComponent>
-            )}
-          </div>
-        }
+        <TetherComponent
+          ref={tether => {
+            this.tether = tether;
+          }}
+          attachment="top center"
+          renderTarget={ref => (
+              <div ref={ref} >
+                <EmojiButton />
+              </div>
+          )}
+          renderElement={ref =>
+            this.props.displayPicker && (
+              <div ref={ref}>
+                <EmojiPicker />
+              </div>
+            )
+          }
+        />
       </div>
     );
   }
@@ -85,10 +44,11 @@ const mapStateToProps = state => {
   };
 };
 
-
 const mapDispatchToProps = {
   togglePicker,
 };
 
-
-export default connect(mapStateToProps, mapDispatchToProps)(App);
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps,
+)(App);
